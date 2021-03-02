ddf15b36a734d2ec4c735d1079deb3a0a90b6e20
Dymytrii Tretiakov <tretyakov3651@gmail.com>
2021-03-02 10:22:00 +0200

    Added test components

--- TestApp/TestApp/MainPage.xaml.cs
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -10,9 +10,33 @@ namespace TestApp
 {
     public partial class MainPage : ContentPage
     {
+        private int count = 0;
+
         public MainPage()
         {
             InitializeComponent();
+
+            progressBar1.ProgressTo(1, 5000, Easing.BounceOut);
+            progressBar2.ProgressTo(1, 5000, Easing.CubicOut);
+            progressBar3.ProgressTo(1, 5000, Easing.SpringIn);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = $"You clicked {++count} times!";
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            Stepper_Label.Text = value.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            rotatingLabel.Rotation = value;
+            displayLabel.Text = String.Format("The Slider value is {0}", value);
         }
     }
 }
